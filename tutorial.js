057cb7abd26d7a756659d3d6515d535d2a76a249
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2024-05-18 21:24:49 +0800

    Changes UI & UX Responsive

--- tutorial.js
+++ tutorial.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const sidebar = document.querySelector(".sidebar");
   const sidebarClose = document.querySelector("#sidebar-close");
+  const menu = document.querySelector(".menu-content");
   const menuItems = document.querySelectorAll(".submenu-item");
   const subMenuTitles = document.querySelectorAll(".submenu .menu-title");
+  const sidebarLinks = document.querySelectorAll(".sidebar a");
+  const submenuItems = document.querySelectorAll(".submenu .item");
 
   // Toggle sidebar
   sidebarClose.addEventListener("click", () => {
@@ -14,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Open submenu
   menuItems.forEach((item, index) => {
     item.addEventListener("click", () => {
-      sidebar.classList.remove("close"); // Ensure sidebar is open when submenu is active
+      menu.classList.add("submenu-active");
       item.classList.add("show-submenu");
       menuItems.forEach((item2, index2) => {
         if (index !== index2) {
@@ -27,9 +30,35 @@ document.addEventListener("DOMContentLoaded", () => {
   // Close submenu
   subMenuTitles.forEach((title) => {
     title.addEventListener("click", () => {
+      menu.classList.remove("submenu-active");
       menuItems.forEach((item) => {
         item.classList.remove("show-submenu");
       });
+    });
+  });
+
+  // Ensure all links within the sidebar and submenu are clickable
+  sidebarLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      if (sidebar.classList.contains("close")) {
+        e.preventDefault();
+        sidebar.classList.remove("close");
+        setTimeout(() => {
+          window.location.href = link.href;
+        }, 300);
+      }
+    });
+  });
+
+  submenuItems.forEach((submenuLink) => {
+    submenuLink.addEventListener("click", (e) => {
+      if (sidebar.classList.contains("close")) {
+        e.preventDefault();
+        sidebar.classList.remove("close");
+        setTimeout(() => {
+          window.location.href = submenuLink.querySelector("a").href;
+        }, 300);
+      }
     });
   });
 });
