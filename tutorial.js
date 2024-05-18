17347e4e190888b55530f325fe891e78206a0c91
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2024-05-18 21:20:40 +0800

    Changes UI & UX for Responsive Screens

--- tutorial.js
+++ tutorial.js
@@ -1,25 +1,35 @@
-const sidebar = document.querySelector(".sidebar");
-const sidebarClose = document.querySelector("#sidebar-close");
-const menu = document.querySelector(".menu-content");
-const menuItems = document.querySelectorAll(".submenu-item");
-const subMenuTitles = document.querySelectorAll(".submenu .menu-title");
+document.addEventListener("DOMContentLoaded", () => {
+  const sidebar = document.querySelector(".sidebar");
+  const sidebarClose = document.querySelector("#sidebar-close");
+  const menuItems = document.querySelectorAll(".submenu-item");
+  const subMenuTitles = document.querySelectorAll(".submenu .menu-title");
 
-sidebarClose.addEventListener("click", () => sidebar.classList.toggle("close"));
+  // Toggle sidebar
+  sidebarClose.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+    document.querySelector(".main").classList.toggle("full-width");
+    document.querySelector(".navbar").classList.toggle("full-width");
+  });
 
-menuItems.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    menu.classList.add("submenu-active");
-    item.classList.add("show-submenu");
-    menuItems.forEach((item2, index2) => {
-      if (index !== index2) {
-        item2.classList.remove("show-submenu");
-      }
+  // Open submenu
+  menuItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      sidebar.classList.remove("close"); // Ensure sidebar is open when submenu is active
+      item.classList.add("show-submenu");
+      menuItems.forEach((item2, index2) => {
+        if (index !== index2) {
+          item2.classList.remove("show-submenu");
+        }
+      });
     });
   });
-});
 
-subMenuTitles.forEach((title) => {
-  title.addEventListener("click", () => {
-    menu.classList.remove("submenu-active");
+  // Close submenu
+  subMenuTitles.forEach((title) => {
+    title.addEventListener("click", () => {
+      menuItems.forEach((item) => {
+        item.classList.remove("show-submenu");
+      });
+    });
   });
 });
